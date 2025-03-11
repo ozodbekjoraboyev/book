6bf6e7640af60f114411b57e462f0efd3dffdb41
unknown <baxtiyor.gazimboyev@gmail.com>
2025-03-11 21:58:20 +0300

    codeWArs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,18 @@
 "use client";
-import axios from "axios";
-import { useEffect, useState } from "react";
+
+import Image from "next/image";
+
+const images = Array.from({ length: 20 }, (_, i) => ({
+  src: require(`../../public/rasm${i + 1}.png`),
+  alt: `rasm${i + 1}`,
+}));
 
 export default function Home() {
-  const [book, setBook] = useState<
-    {
-      author: {
-        name: string;
-      };
-      deletedAt: null;
-      description: null;
-      id: number;
-      image: string;
-      name: string;
-      stocks: {
-        busy: boolean;
-        id: number;
-        locationId: number;
-      }[];
-    }[]
-  >();
-  useEffect(() => {
-    axios
-      .get(`https://library.softly.uz/api/app/books?size=20&page=1&order=DESC`)
-      .then((res) => {
-        console.log(res.data.items);
-        setBook(res.data.items);
-      });
-  }, []);
   return (
-    <>
-      <div className="grid grid-cols-5 gap-5 ">
-        {book?.map((item) => {
-          return (
-            <div key={item.id} className=" bg-white text-black rounded p-2 ">
-              <img
-                className="h-[300px] w-[250px] m-auto rounded-xl"
-                src={item.image}
-                alt=""
-              />
-              <div>
-                <p>{item.name}</p>
-                <p className=" opacity-70">{item.author.name}</p>
-                <p></p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div className="grid grid-cols-4 gap-4 container mx-auto p-4">
+      {images.map((img, index) => (
+        <Image key={index} width={300} height={400} src={img.src} alt={img.alt} className=" bg-origin-border" />
+      ))}
+    </div>
   );
 }
